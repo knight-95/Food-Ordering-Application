3902a0f5b25c5c5383725740a1751b35dd0fe6f3
Yash Gupta <yashgupta13.in@gmail.com>
2023-02-03 23:51:12 +0530

    Updated basic features

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
-import Image from 'next/image'
-import styles from "../styles/Featured.module.css"
+import React from "react";
+import Image from "next/image";
+import styles from "../styles/Featured.module.css";
 
 const Featured = () => {
+  const images = [
+    "/img/featured.png",
+    "/img/featured2.png",
+    "/img/featured3.png",
+  ];
+
   return (
     <div className={styles.container}>
-    <Image scr="/img/arrow1.png" alt=""/>
-    <div className={styles.wrappe}>
-
-    </div>
-    <Image scr="/img/arrowr.png" alt=""/>
+      <div className={styles.arrowContainer}>
+        <Image scr="/img/arrowl.png"  alt="" />
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.imgContainer}>
+          {images.map((img, i) => (
+            <Image src="/img/featured.png" fill key={i} alt="" />
+          ))}
+        </div>
+      </div>
+      <div className={styles.arrowContainer}>
+        <Image scr="/img/arrowr.png" alt="" />
+      </div>
     </div>
   );
-}
+};
 
-export default Featured
+export default Featured;
